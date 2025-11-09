97a6722abfc856de1a9254519294865448396faa
Polceze <paulo.odera@gmail.com>
2025-11-09 11:08:11 +0300

    Corrected a typo

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/user.js';
 
 // --- 1. Middleware to protect routes (Authentication) ---
 export const protect = async (req, res, next) => {
